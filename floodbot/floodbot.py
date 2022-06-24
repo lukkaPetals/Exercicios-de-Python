9232c0b0f7db7998c9c20bdee278ce446c2b5f76
lukkaPetals <107210315+lukkaPetals@users.noreply.github.com>
2022-06-23 21:16:15 -0300

    Versão final verdadeira tava brincando´, é prank pô

--- floodbot/floodbot.py
+++ floodbot/floodbot.py
@@ -5,7 +5,7 @@ quant = int(input(f"Digite quantas vezes você quer marcar {arroba}: "))
 print("O bot ira funcionar daqui 5 segundos aguarde...")
 time.sleep(5)
 print("O bot está funcionando no momento")
-for c in range(0, quant + 1):
+for c in range(0, quant):
     pyautogui.typewrite(arroba)
     time.sleep(0.3)
     for d in range(0, 3):
